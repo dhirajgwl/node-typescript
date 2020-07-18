2436c2072ecb9dfe4ea83169d09fb43f330b3d8c
Dheeraj <dhiraj.gupta82@gmail.com>
2020-07-18 16:40:33 +0900

    Fixed defects

--- app/src/page/index.tsx
+++ app/src/page/index.tsx
@@ -1,12 +1,13 @@
 import React,{useState,Fragment} from "react";
 import axios from 'axios';
+import ModalWindow from "../components/modalWindow";
 import {getMessage,Error} from '../common/getErrorMsg'
 import TextArea from "../components/textArea";
 import TextInput from "../components/textInput";
 import Button from "../components/button";
 import Card from "../components/Card";
 import './style.scss';
-import ModalWindow from "../components/modalWindow";
+
 
 export type InitialState  = {
    success:boolean;
@@ -25,12 +26,16 @@ const initialState  = {
  } 
 
 const Page = () =>{
-   const [{success,open,userName,wish,message},setState] = useState<InitialState>(initialState)
+   const [{success,open,userName,wish,message},setState] = useState<InitialState>(initialState);
   
    const onFormSubmit = (evt:React.MouseEvent <HTMLButtonElement>) =>{
       evt.preventDefault();
-      if(!userName) return setState({...initialState,open:true,success:false,message:getMessage(Error.USER_EMPTY)})
-      if(!wish) return setState({...initialState,open:true,success:false,message:getMessage(Error.WISH_EMPTY)})
+      if(!userName) {
+         return setState(prevState  => ({...prevState,wish,open:true,success:false,message:getMessage(Error.USER_EMPTY)}));
+      }
+      if(!wish) {
+         return setState({...initialState,userName,open:true,success:false,message:getMessage(Error.WISH_EMPTY)});
+      } 
       sendWish();
    }
    const sendWish = async() =>{
@@ -57,7 +62,15 @@ const Page = () =>{
             <Button text="Send" onClick={onFormSubmit}></Button>
           </form>
          </Card>
-         <ModalWindow open={open} success={success} handlePopup={()=>{setState({ ...initialState });}}>
+         <ModalWindow open={open} success={success} handlePopup={()=>{
+            if(Error.USER_EMPTY){
+               return  setState(prevState  => ({...prevState,open:false,success:false,message:''}));
+            }
+            if(Error.WISH_EMPTY){
+               return  setState(prevState  => ({...prevState,open:false,success:false,message:''}));
+            }
+            setState({ ...initialState });
+            }}>
             <p>{message}</p>
          </ModalWindow>
       </Fragment>)
